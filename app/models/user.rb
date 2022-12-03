76feb387eb70f8c76b2a011e3c9a4d37d9260889
Todd Perkins <fullstacktodd@gmail.com>
2022-12-03 05:07:32 -0500

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,15 @@ class User < ApplicationRecord
   # -----------------------------------------------------
   # users i am following
   def self.followed_users(current_user)
-
+    
     return self.where(id: current_user.follows.map { |f| f.follower_id })
-
+    
   end
 
   # -----------------------------------------------------
   # users i am NOT following
   def self.not_followed_users(current_user)
-
+    
     # create array of user ids that we are not following
     not_following_ids = self.all.map { |u| u.id } - current_user.follows.map { |f| f.follower_id }.insert(0, current_user.id)
     
